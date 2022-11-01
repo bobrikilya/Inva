03863a30301cf427c1bc75433ca4cf1530447bc9
bobrikilya <ilya.bobrik.2020@gmail.com>
2022-11-01 15:15:46 +0300

    Added some colors & icons for docs

--- Barcode_scanner/js/script.js
+++ Barcode_scanner/js/script.js
@@ -331,7 +331,7 @@ document.addEventListener('click', (event) => {
         const sessions_cont_inside = event.composedPath().includes(all_sessions_cont);
         const session_but_inside = event.composedPath().includes(session_but);
 
-            if (!sessions_cont_inside && !session_but_inside) {
+            if (!sessions_cont_inside && !session_but_inside) { 
                 all_sessions_cont.classList.remove('toggle');
                 all_sess_cont_content.classList.remove('toggle');
             };
